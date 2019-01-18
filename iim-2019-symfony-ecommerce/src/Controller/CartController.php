b4bb31046c7ec3097db2fe65e46355736a011b72
Antoine Gouteix <antoinegouteix@gmail.com>
2019-01-18 12:48:16 +0000

    Add product to cart

--- iim-2019-symfony-ecommerce/src/Controller/CartController.php
+++ iim-2019-symfony-ecommerce/src/Controller/CartController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Cart;
+use App\Entity\CartProduct;
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
@@ -38,10 +41,12 @@ class CartController extends AbstractController
     /**
      * @Route("/cart/add.json", name="add_cart_json", methods={"POST"})
      */
-    public function addToCartJson(Request $request)
+    public function addToCartJson(Request $request, SessionInterface $session)
     {
         $repositoryP = $this->getDoctrine()->getRepository(Product::class);
         $product     = $repositoryP->find($request->request->get('product_id'));
+
+        $objectManager = $this->getDoctrine()->getManager();
 
         if (!$product instanceof Product) {
             $status  = 'ko';
@@ -51,6 +56,29 @@ class CartController extends AbstractController
                 $status  = 'ko';
                 $message = 'Missing quantity for product';
             } else {
+                $cartId = $session->get('cart');
+
+                if (!$cartId) {
+                    $cart = new Cart();
+
+                    $objectManager->persist($cart);
+                    $objectManager->flush();
+
+                    $session->set('cart', $cartId = $cart->getId());
+                } else {
+                    $repositoryCart = $this->getDoctrine()->getRepository(Cart::class);
+                    /** @var Cart $cart */
+                    $cart = $repositoryCart->find($cartId);
+                }
+
+                $cartProduct = new CartProduct();
+                $cartProduct->setCart($cart);
+                $cartProduct->setProduct($product);
+                $cartProduct->setQuantity((int)$request->request->get('quantity'));
+
+                $objectManager->persist($cartProduct);
+                $objectManager->flush();
+
                 $status  = 'ok';
                 $message = 'Added to cart';
             }
@@ -59,6 +87,20 @@ class CartController extends AbstractController
         return new JsonResponse([
             'result'  => $status,
             'message' => $message,
+        ]);
+    }
+
+    public function partial(SessionInterface $session)
+    {
+        $cartId = $session->get('cart');
+
+        $repositoryCart = $this->getDoctrine()->getRepository(Cart::class);
+
+        /** @var Cart $cart */
+        $cart = $cartId ? $repositoryCart->find($cartId) : new Cart();
+
+        return $this->render('partials/cart.html.twig', [
+            'cart' => $cart
         ]);
     }
 }
