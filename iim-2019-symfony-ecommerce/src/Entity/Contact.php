f0882de44d7568359d1184c2d5a6be1ff4a4c8b7
Antoine Gouteix <antoinegouteix@gmail.com>
2019-01-31 22:33:15 +0000

    Ajout contact

---
+++ iim-2019-symfony-ecommerce/src/Entity/Contact.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Asserts;
+
+class Contact {
+    
+    /**
+     * @var string & null
+     * Assert\NotBlank()
+     * Assert\Length(min=2, max=100)
+     */
+     private $firstname;
+     
+      /**
+     * @var string & null
+     * Assert\NotBlank()
+     * Assert\Length(min=2, max=100)
+     */
+     private $lastname;
+     
+      /**
+     * @var string & null
+     * Assert\NotBlank()
+     * Assert\Regex(
+     * pattern="/[0-9]{10}/"
+     * )
+     */
+     private $phone;
+     
+      /**
+     * @var string & null
+     * Assert\NotBlank()
+     * Assert\Email()
+     */
+     private $email;
+     
+     /**
+     * @var string & null
+     * Assert\NotBlank()
+     * Assert\Length(min=10)
+     */
+     private $message;
+     
+     /**
+     * @var string & null
+     * Assert\NotBlank()
+     */
+     private $product;
+    
+}
